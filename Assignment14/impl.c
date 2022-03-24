51040d983536807099cc131eb8aa4af7c7ec6a4b
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-03-24 14:13:15 +0100

    Update impl.c

--- Assignment14/impl.c
+++ Assignment14/impl.c
@@ -32,6 +32,27 @@ void destroy() {
 	free(matrix);
 
 	printf("Matrix Destroyed (Memory DeAllocated)\n");
+}
+bool insert(char* str) {
+	/* pre : str!=null and contains a string STR (\0 as sentinel), not
+		 necessary dynamically allocated, storage exists
+   post: string STR stored in an available free storage slot and 1
+		 returned, or 0 returned if there are no free storage slots.
+*/
+	if (strcmp(str, "") == 0)
+	{
+		printf("Null not accepted");
+		return;
+	}
+	int length = sizeof(str);
+	if (strcmp(str[length - 1], "\0") == 0)
+	{
+		//continue
 
+	}
+	else {
+		printf("sentinel not present.");
+		return;
+	}
 }
 
