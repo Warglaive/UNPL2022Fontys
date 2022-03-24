dd89fddb2f0bd6ea5134c11f27e22fa0c968a8fd
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-03-24 11:56:10 +0100

    prereq

--- Assignment14/impl.c
+++ Assignment14/impl.c
@@ -1,14 +1,34 @@
 #include "arraydatastore.h"
 #define row 40
-//char matrixStorage[NUMBER_OF_STRING][MAX_STRING_SIZE];
-#define NUMBER_OF_STRING
 
 char** matrix;
 void init(int col) {
+	if (col <= 0)
+	{
+		printf("col has to be > 0!");
+		return;
+	}
 	char** matrix = (char**)malloc(row * sizeof(char*));
 
 	for (int rowCounter = 0; rowCounter < row; rowCounter++)
 	{
 		matrix[rowCounter] = malloc(col * sizeof(char));
 	}
+	printf("Matrix initialized (Memory allocated)\n");
+
 }
+void destroy() {
+	/* pre : true
+	   post: allocated memory de-allocated
+	*/
+	//char** matrix = (char**)malloc(row * sizeof(char*));
+
+	for (int rowCounter = 0; rowCounter < sizeof(matrix); rowCounter++)
+	{
+		matrix[rowCounter] = malloc(col * sizeof(char));
+	}
+
+	printf("Matrix Destroyed (Memory DeAllocated)\n");
+
+}
+
--- Assignment14/main.c
+++ Assignment14/main.c
@@ -2,6 +2,5 @@
 int main() {
 	int stringsCount = 4;
 	init(stringsCount);
-	printf("end of main");
-//	insert(char* string);
+
 }
