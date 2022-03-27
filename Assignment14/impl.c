668249b3319a55d848799a6f4885664d7d976e44
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-03-27 17:35:42 +0200

    Update impl.c
    test

--- Assignment14/impl.c
+++ Assignment14/impl.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define row 40
+#define row 4
 // used to store a dynamically allocated array of pointers to char
 
 char** strings;
-
+int col;
 void init(int n) {
 	if (n <= 0)
 	{
 		printf("col has to be > 0!");
 		return;
 	}
+	col = n;
 	strings = malloc(row * sizeof(char*));
 
 	for (int rowCounter = 0; rowCounter < row; rowCounter++)
 	{
-		strings[rowCounter] = malloc(n * sizeof(char*));
+		strings[rowCounter] = (char*)malloc(n * sizeof(char*));
 	}
 	printf("Matrix initialized (Memory Allocated)\n");
 
@@ -44,25 +45,46 @@ post: string STR stored in an available free storage slot and 1
 */
 bool insert(char* str) {
 
-	int inputLength = strlen(strings);
-	if (inputLength > 0)
-	{
-		for (int i = 0; i < row; i++)
-		{
-			//if last element is reached - return;
-			if (strings[i] == '\0')
-			{
-				return;
-			}
-			else
-			{
-				// dynamically allocate enough space to store the the string, store the 
-				// pointer to this block of memory in strings[i]
-				strings[i] = malloc(inputLength * sizeof(char));
-				strcpy(strings[i], str);
-			}
+	int inputLength = strlen(str);
+	strings[0] = malloc(inputLength * sizeof(char));
+	strcpy(strings[0], "1");
 
-		}
+	strings[1] = malloc(inputLength * sizeof(char));
+	strcpy(strings[1], "2");
+
+	strings[2] = malloc(inputLength * sizeof(char));
+	strcpy(strings[2], "3");
+
+	strings[3] = malloc(inputLength * sizeof(char));
+	strcpy(strings[3], "4");
+
+	for (int i = 0; i < col; i++)
+	{
+		printf("%s", strings[i]);
+
 	}
+
+
+
+
+
+
+	for (int i = 0; i < row; i++)
+	{
+		//if last element is reached - return;
+		if (strings[i] == '\0')
+		{
+			return;
+		}
+		else
+		{
+			// dynamically allocate enough space to store the the string, store the 
+			// pointer to this block of memory in strings[i]
+			strings[i] = malloc(inputLength * sizeof(char));
+			strcpy(strings[i], str);
+		}
+
+	}
+
 }
 
