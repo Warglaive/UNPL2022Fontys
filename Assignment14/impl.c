bef2cf7bb64313a3db90dc5545cd110d9eccbb3e
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-03-27 16:55:36 +0200

    Update impl.c

--- Assignment14/impl.c
+++ Assignment14/impl.c
@@ -3,52 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 #define row 40
+// used to store a dynamically allocated array of pointers to char
 
-char** matrix;
+char** strings;
 void init(int n) {
 	if (n <= 0)
 	{
 		printf("col has to be > 0!");
 		return;
 	}
-	char** matrix = (char**)malloc(row * sizeof(char*));
+	strings = (char**)malloc(row * sizeof(char*));
 
 	for (int rowCounter = 0; rowCounter < row; rowCounter++)
 	{
-		matrix[rowCounter] = malloc(n * sizeof(char));
+		strings[rowCounter] = malloc(n * sizeof(char*));
 	}
 	printf("Matrix initialized (Memory Allocated)\n");
 
 }
+/* pre : true
+   post: allocated memory de-allocated
+*/
 void destroy() {
-	/* pre : true
-	   post: allocated memory de-allocated
-	*/
 	//char** matrix = (char**)malloc(row * sizeof(char*));
 	//for (int rowCounter = 0; rowCounter < sizeof(matrix); rowCounter++)
 	//{
 	//	printf("in %d", rowCounter);
 	//	//free(matrix[rowCounter]);
 	//}
-	free(matrix);
+	free(strings);
 
 	printf("Matrix Destroyed (Memory DeAllocated)\n");
 }
-bool insert(char* str) {
-	/* pre : str!=null and contains a string STR (\0 as sentinel), not
-		 necessary dynamically allocated, storage exists
-   post: string STR stored in an available free storage slot and 1
-		 returned, or 0 returned if there are no free storage slots.
+
+/* pre : str!=null and contains a string STR (\0 as sentinel), not
+	 necessary dynamically allocated, storage exists
+post: string STR stored in an available free storage slot and 1
+	  returned, or 0 returned if there are no free storage slots.
 */
-	malloc(strlen(str)+1);
-	int length = strlen(str) - 1;
-	printf("%d", length);
+bool insert(char* str) {
+
+	int length = strlen(str);
+
+	char* inputDynAlloc = malloc(length * sizeof(char));
+	inputDynAlloc[length] = '0';
+	printf("%s", *inputDynAlloc);
 	if (strcmp(str, "") == 0)
 	{
 		printf("Null not accepted");
 		return 1;
 	}
-	char lastChar = str[length]+1;
+	char lastChar = str[length];
 	if (strcmp(str[length - 1], "\\0") == 0)
 	{
 		//continue
@@ -59,9 +64,9 @@ bool insert(char* str) {
 
 		//
 	}
-	else {
+	/*else {
 		printf("sentinel not present.");
 		return;
-	}
+	}*/
 }
 
